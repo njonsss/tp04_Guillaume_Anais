15439e8fde91aa78ce241228dfc48b885cdb1280
njonsss <senelah960@gmail.com>
2020-10-26 00:49:45 +0100

    5 files modified, 1 added, 1 deleted

--- src/app/hearder/header.component.ts
+++ src/app/hearder/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Cart } from "../interface/cart";
+import { CartService } from "../services/cart.service";
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  cart$: Observable<Cart>;
 
-  constructor() { }
+  constructor(private CartService: CartService) {}
 
   ngOnInit() {
+    this.cart$ = this.CartService.getCart();
   }
-
 }
